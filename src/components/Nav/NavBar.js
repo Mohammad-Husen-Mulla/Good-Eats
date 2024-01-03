1d2407f844d5e4b962076413e41a5be3118f7f1d
Mohammad-Husen-Mulla <mohammadmulla29@gmail.com>
2024-01-03 13:15:46 +0530

    dev: update the nav-bar

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -1,11 +1,19 @@
 
 import style from './NavBar.module.css';
-import logo from '../../img/good-eats-logo-removebg-latest.png';
 import './icon.css';
 
 const NavBar = () => {
   return <section className={style['nav-section']}>
-
+    <div className={style['nav-logo-menu-container']}>
+      <div className={style['nav-bar__left']}>
+        <p className={style['nav-logo__name-plate']}>Good Eats</p>
+        <ion-icon class="menu-icon" name="menu-outline"></ion-icon>
+      </div>
+      <div className={style['nav-bar__right']}>
+        <p className={style['nav-bar__contact']}>+86 852 346 000</p>
+        <button className={style['nav-bar__reservation-btn']}>Reservations</button>
+      </div>
+    </div>
   </section>
 };
 
