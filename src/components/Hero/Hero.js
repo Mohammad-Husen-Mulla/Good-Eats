c2b5b7a56343ea865036673019c419d2e54aeae5
Mohammad-Husen-Mulla <mohammadmulla29@gmail.com>
2024-01-03 16:00:28 +0530

    dev: complete hero verion 1

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,16 @@
 import style from "./Hero.module.css";
+import hero from '../../img/good-eats-hero.jpg'
 const Hero = () => {
   return <section className={style["hero-section"]}>
-
+    <div className={style['hero-text__container']}>
+      <p className={style['hero-text__main']}>Healthy Eating
+        is important
+        part of lifestyle</p>
+      <p className={style['hero-text__description']}>Best Restaurant in the town..!</p>
+    </div>
+    <div className={style['hero-main-image__container']}>
+      <img src={hero} alt="burger" />
+    </div>
   </section>
 };
 
