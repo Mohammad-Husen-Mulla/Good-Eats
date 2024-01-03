0629c0a9abb0bde22002c64edc8647c05d8f1745
Mohammad-Husen-Mulla <mohammadmulla29@gmail.com>
2024-01-03 19:37:49 +0530

    dev: complete hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,9 @@
 import style from "./Hero.module.css";
-import hero from '../../img/good-eats-hero.jpg'
+import hero from '../../img/good-eats-hero.jpg';
+import plate1 from '../../img/plate-img-1.jpg';
+import plate2 from '../../img/plate-img-2.jpg';
+import plate3 from '../../img/plate-img-3.jpg';
+
 const Hero = () => {
   return <section className={style["hero-section"]}>
     <div className={style['hero-text__container']}>
@@ -17,7 +21,17 @@ const Hero = () => {
       <svg width="2" height="406" viewBox="0 0 2 406" fill="none" xmlns="http://www.w3.org/2000/svg">
         <line x1="1" y1="1" x2="1.00002" y2="405" stroke="white" stroke-width="2" stroke-linecap="square" stroke-linejoin="round" stroke-dasharray="8 8" />
       </svg>
-
+    </div>
+    <div className={style['hero-recipe-plate__container']}>
+      <div>
+        <img src={plate2} alt="plate2" />
+      </div>
+      <div>
+        <img src={plate1} alt="plate1" />
+      </div>
+      <div>
+        <img src={plate3} alt="plate3" />
+      </div>
     </div>
   </section>
 };
