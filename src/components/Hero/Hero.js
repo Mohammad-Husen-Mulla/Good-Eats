ba18173e5cb102e9a83c9da5565fcd02ffdaf856
Mohammad-Husen-Mulla <mohammadmulla29@gmail.com>
2024-01-03 16:50:16 +0530

    fix: fix hero section alignment

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,6 +11,14 @@ const Hero = () => {
     <div className={style['hero-main-image__container']}>
       <img src={hero} alt="burger" />
     </div>
+    <div className={style["hero-scroll__container"]}>
+
+      <p>Scroll</p>
+      <svg width="2" height="406" viewBox="0 0 2 406" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <line x1="1" y1="1" x2="1.00002" y2="405" stroke="white" stroke-width="2" stroke-linecap="square" stroke-linejoin="round" stroke-dasharray="8 8" />
+      </svg>
+
+    </div>
   </section>
 };
 
